003b13e1c1baad3e63bb14f904f7c512e8dfab68
Divyanshu Maithani <div.blackcat@gmail.com>
2017-06-12 22:55:40 +0530

    NPM 5 package-lock.json :raised_hands:

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.get('/api/screenshot/:number', bot.screenshot);
 
 
 app.listen(app.get('port'), () => {
-	console.log('%s App is running on port %s mode', colors.green('✓'), app.get('port'), app.get('env')); 
+	console.log('%s App is running on port %s mode', colors.green('✓'), app.get('port'), app.get('env'));
 	console.log('  Press CTRL-C to stop\n');
 });
 
